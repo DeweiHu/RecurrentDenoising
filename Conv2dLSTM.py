6cb480377ad6bc0772dbe780695d23c8c369df5e
Dewei Hu <50162880+DeweiHu@users.noreply.github.com>
2020-09-07 16:59:20 -0500

    Add files via upload

--- Conv2dLSTM.py
+++ Conv2dLSTM.py
@@ -22,7 +22,6 @@ class ConvLSTMCell(nn.Module):
         self.input_channels = input_channels
         self.hidden_channels = hidden_channels
         self.kernel_size = kernel_size
-        self.num_features = 4
         
         self.padding = int((kernel_size - 1) / 2)
         
@@ -49,8 +48,8 @@ class ConvLSTMCell(nn.Module):
         return ch, cc
 
     def init_hidden(self, batch_size, hidden, shape):
-        return (Variable(torch.zeros(batch_size, hidden, shape[0], shape[1], shape[2])).cuda(),
-                Variable(torch.zeros(batch_size, hidden, shape[0], shape[1], shape[2])).cuda())
+        return (Variable(torch.zeros(batch_size, hidden, shape[0], shape[1])).cuda(),
+                Variable(torch.zeros(batch_size, hidden, shape[0], shape[1])).cuda())
 
 
 class ConvLSTM(nn.Module):
@@ -65,7 +64,7 @@ class ConvLSTM(nn.Module):
         self.kernel_size = kernel_size
         
         # number of convLSTM cells stacked
-        self.num_layers = len(hidden_channels)
+        self.seq_dim = len(hidden_channels)
         
         # when step == 0, do initialization, step == 1, forward propagation
         self.step = step
@@ -73,7 +72,7 @@ class ConvLSTM(nn.Module):
         self._all_layers = []
         
         # define LSTM cells
-        for i in range(self.num_layers):
+        for i in range(self.seq_dim):
             name = 'cell{}'.format(i)
             cell = ConvLSTMCell(self.input_channels[i], self.hidden_channels[i], self.kernel_size)
             
@@ -84,8 +83,8 @@ class ConvLSTM(nn.Module):
     def forward(self, x):
         internal_state = []
         outputs = []
-        for step in range(self.step):
-            for i in range(self.num_layers):
+        for step in range(self.step+1):
+            for i in range(self.seq_dim):
                 # Initialize cells at the first step
                 name = 'cell{}'.format(i)
                 if step == 0:
@@ -102,18 +101,20 @@ class ConvLSTM(nn.Module):
             # only record effective steps
             if step in self.effective_step:
                 outputs.append(x)
+        # output a tuple ()
         return outputs, (x, new_c)
-    
+
+#%% Model Check  
 if __name__ == '__main__':
     # gradient check
-    convlstm = ConvLSTM(input_channels=512, hidden_channels=[128, 64, 64, 32, 32], kernel_size=3, step=5,
-                        effective_step=[4]).cuda()
-    loss_fn = torch.nn.MSELoss()
+    convlstm = ConvLSTM(input_channels=1,hidden_channels=[32,64,64,32,1],kernel_size=3,step=1,
+                        effective_step=[1]).cuda()
+    criterion = torch.nn.MSELoss()
 
     x = Variable(torch.randn(1,1,64,64)).cuda()
-    y = Variable(torch.randn(1,32,64,64)).double().cuda()
+    y = Variable(torch.randn(1,1,64,64)).double().cuda()
 
     y_hat = convlstm(x)
     y_hat = y_hat[0][0].double()
     
-    res = torch.autograd.gradcheck(loss_fn,(y_hat,y),eps=1e-6,raise_exception=True)
+    res = torch.autograd.gradcheck(criterion,(y_hat,y),eps=1e-6,raise_exception=True)
