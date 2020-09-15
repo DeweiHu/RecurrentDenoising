e32d21adfac28b035a934eabb90aa42737bd3093
Dewei Hu <50162880+DeweiHu@users.noreply.github.com>
2020-09-15 17:21:27 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep  7 17:30:14 2020
+Created on Sun Sep 13 19:15:32 2020
 
-@author: hudew
+@author: hud4
 """
 
 import sys
-sys.path.insert(0,'C:\\Users\\hudew\\OneDrive\\桌面\\RNN\\')
-from Conv2dLSTM import ConvLSTM
+sys.path.insert(0,'/home/hud4/Desktop/2020/RNN/')
+import Conv2dLSTM
 import util
 
-import time,os
+import pickle,time
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -23,27 +24,28 @@ import torchvision.transforms as transforms
 from torch.optim.lr_scheduler import StepLR
 from torch.autograd import Variable
 
-# dataloader
-dataroot = 'E:\\HumanData\\'
-trainlist = []
+dataroot = '/home/hud4/Desktop/2020/Data/'
 
-for file in os.listdir(dataroot):
-    if file.endswith("_2.nii"):
-        trainlist.append(file)
+# Model
+input_ch = 1
+hidden_ch = [4,8,16,32,1]
+kernel_size = (3,3)
+num_layers = 5
 
-#%%
-#plt.figure(figsize=(12,12))
-#plt.imshow(data[200:700,33:533,1000],cmap='gray')
-#plt.imshow()         
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-#%% 
-global NumFrame
-NumFrame = 5
+model = Conv2dLSTM.DN_LSTM(input_ch,hidden_ch,kernel_size,num_layers,True,True,False)
+model = model.to(device)
 
+#check parameter number
+#for i in range(len(list(model.parameters()))):
+#    print(list(model.parameters())[i].size())
+
+#%% DataLoader
 batch_size = 1
 
-# [(x,y)], x:[batch,seq_dim,channel,H,W]
-
+# x:[batch,seq_dim,channel,H,W]
+# y:[batch,channel,H,W]
 class MyDataset(Data.Dataset):
 
     def ToTensor(self, x, y):
@@ -53,69 +55,78 @@ class MyDataset(Data.Dataset):
         return x_tensor, y_tensor    
         
     def __init__(self, dir):
-       self.data = util.nii_loader(dir)
-       self.data = util.ImageRescale(np.float32(np.transpose(self.data,[2,1,0])),[0,255])
-       self.data = self.data[:100,200:700,33:533]
-       
-       self.pair = ()
-       for i in range(100):
-           if i % NumFrame == 0:
-               x = self.data[i:i+NumFrame,:,:]
-               y = np.mean(x,axis=0)
-               self.pair = self.pair + ((x,y),)
+       with open(dir,'rb') as f:
+           self.pair = pickle.load(f)
         
     def __len__(self):
         return len(self.pair)
 
     def __getitem__(self,idx):
-        (x, y) = self.pair[idx]
-        x_tensor, y_tensor = self.ToTensor(x,y)
+        x, y = self.pair[idx]
+        x_tensor, y_tensor = self.ToTensor(x,util.ImageRescale(y,[0,1]))
         return x_tensor, y_tensor
     
-train_loader = Data.DataLoader(dataset=MyDataset(dataroot+trainlist[0]), 
+train_loader = Data.DataLoader(dataset=MyDataset(dataroot+'train.pickle'), 
                                batch_size=batch_size, shuffle=True)
 
-
-#%%
+# dimension check
 #for step,(x,y) in enumerate(train_loader):
 #    pass
 #print(x.size())
 #print(y.size())
-
-#%% Model
-# initialization 
-model = ConvLSTM(input_dim = 1, hidden_dim = 1,
-                 kernel_size = (3,3), num_layers = 3,
-                 batch_first = True, bias = True,
-                 return_all_layers = False).cuda()
-
-for i in range(len(list(model.parameters()))):
-    print(list(model.parameters())[i].size())
     
 #%% loss
-num_epoch = 10
-criterion = nn.MSELoss().cuda()
-learning_rate = 0.1
+num_epoch = 50
+
+criterion1 = nn.L1Loss()
+criterion2 = nn.MSELoss()
+
+learning_rate = 0.005
 optimizer = torch.optim.Adam(model.parameters(),
                              lr=learning_rate,
                              weight_decay=1e-4)
+scheduler = StepLR(optimizer, step_size=5, gamma=0.5)
+r = 2
+
+sum_loss = []
+
+t1 = time.time()
 
 for epoch in range(num_epoch):
+    epoch_loss = 0
     for step,(x,y) in enumerate(train_loader): 
-        _, last_states = model(Variable(x).cuda(),None)
-        y_hat = last_states[0][0]
+        model.train()
+        
+        h_pre,h_post,y_hat = model(Variable(x).to(device))
         
         # loss and backpropagation
-        loss = criterion(y_hat,Variable(y).cuda())
+        loss1 = criterion1(y_hat,Variable(y).to(device))
+        loss2 = criterion2(y_hat,Variable(y).to(device))
+        loss = loss1+loss2
+        epoch_loss += loss
+        
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         
-        if step % 10 == 0:
-            print(step)
+        if step % 100 == 0:
+            print('loss:{}'.format(loss.item()))
+        
+        if step % 1000 == 0:
+            im_hat = y_hat[0,0,:,:].detach().cpu().numpy()
+            im_pre = h_pre[0,0,:,:].detach().cpu().numpy()
+            im_post = h_post[0,0,:,:].detach().cpu().numpy()
+            im_y = y[0,0,:,:].detach().cpu().numpy()
+            
+            plt.figure(figsize=(16,16))
+            plt.subplot(2,2,1),plt.axis('off'),plt.imshow(im_pre,cmap='gray')
+            plt.subplot(2,2,2),plt.axis('off'),plt.imshow(im_post,cmap='gray')
+            plt.subplot(2,2,3),plt.axis('off'),plt.imshow(im_hat,cmap='gray')
+            plt.subplot(2,2,4),plt.axis('off'),plt.imshow(im_y,cmap='gray')
+            plt.show()
+            
+    sum_loss.append(epoch_loss)
+    scheduler.step()
+t2 = time.time()
 
-    
-    
-    
-
-
+print('time:{} min'.format((t2-t1)/60))
