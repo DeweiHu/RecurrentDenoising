f258c041549fef5ca84c6c9b4c471ff92ac82b66
Dewei Hu <50162880+DeweiHu@users.noreply.github.com>
2020-10-01 10:53:51 -0500

    Add files via upload

--- ConvLSTM.py
+++ ConvLSTM.py
@@ -17,7 +17,7 @@ kernel_size: [int] square kernel by default
 bias: [boolean] add bias or not
 '''
 class ConvLSTMcell(nn.Module):
-    def __init__(self, nch_x, nch_h, kernel_size, bias):
+    def __init__(self, nch_x, nch_h, kernel_size, bias, device):
         super(ConvLSTMcell, self).__init__()
         
         self.nch_x = nch_x
@@ -26,17 +26,17 @@ class ConvLSTMcell(nn.Module):
         self.bias = bias
         self.padding = int((self.kernel_size-1)/2)
         
-        self.Wxi = nn.Conv2d(self.input_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=self.bias)
-        self.Whi = nn.Conv2d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=self.bias)
+        self.Wxi = nn.Conv2d(self.nch_x, self.nch_h, self.kernel_size, 1, self.padding, bias=self.bias)
+        self.Whi = nn.Conv2d(self.nch_h, self.nch_h, self.kernel_size, 1, self.padding, bias=self.bias)
 
-        self.Wxf = nn.Conv2d(self.input_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=self.bias)
-        self.Whf = nn.Conv2d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=self.bias)
+        self.Wxf = nn.Conv2d(self.nch_x, self.nch_h, self.kernel_size, 1, self.padding, bias=self.bias)
+        self.Whf = nn.Conv2d(self.nch_h, self.nch_h, self.kernel_size, 1, self.padding, bias=self.bias)
 
-        self.Wxc = nn.Conv2d(self.input_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=self.bias)
-        self.Whc = nn.Conv2d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=self.bias)
+        self.Wxc = nn.Conv2d(self.nch_x, self.nch_h, self.kernel_size, 1, self.padding, bias=self.bias)
+        self.Whc = nn.Conv2d(self.nch_h, self.nch_h, self.kernel_size, 1, self.padding, bias=self.bias)
 
-        self.Wxo = nn.Conv2d(self.input_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=self.bias)
-        self.Who = nn.Conv2d(self.hidden_channels, self.hidden_channels, self.kernel_size, 1, self.padding, bias=self.bias)
+        self.Wxo = nn.Conv2d(self.nch_x, self.nch_h, self.kernel_size, 1, self.padding, bias=self.bias)
+        self.Who = nn.Conv2d(self.nch_h, self.nch_h, self.kernel_size, 1, self.padding, bias=self.bias)
         
     def forward(self, x, h, c): 
         ci = torch.sigmoid(self.Wxi(x) + self.Whi(h))
@@ -54,7 +54,7 @@ kernal_size: [tuple] a tuple that contains kernel size of each layer
 bias: [boolean]  
 '''
 class LSTMModel(nn.Module):
-    def __init__(self, nlayer, nch_x, nch_h, kernel_size, bias):
+    def __init__(self, nlayer, nch_x, nch_h, kernel_size, bias, device):
         super(LSTMModel, self).__init__()
         # check layer number 
         if not len(kernel_size) == len(nch_h) == nlayer:
@@ -65,18 +65,23 @@ class LSTMModel(nn.Module):
         self.nch_h = nch_h
         self.kernel_size = kernel_size
         self.bias = bias
+        self.device = device
         
         # len(nch_x, nch_h10, nch_h20, ..., nch_hn0)= n+1 
         self.dim_list = (self.nch_x,) + self.nch_h
         cell_list = []
         for i in range(self.nlayer):
-            self.cell_list.append(ConvLSTMcell(self.dim_list[i], self.dim_list[i+1],
-                                               self.kernel_size[i], self.bias))    
+            cell_list.append(ConvLSTMcell(self.dim_list[i], self.dim_list[i+1],
+                                          self.kernel_size[i], self.bias, device))    
         self.cell_list = nn.ModuleList(cell_list)
     
     # h_, c_ are tuples h_ = (h10,h20,h30,...,hn0)
-    def forward(self, input_tensor, h_, c_):
+    def forward(self, input_tensor):
         n_batch, n_seq, n_ch, H, W = input_tensor.shape
+        
+        # initialize states
+        h_, c_ = self.init_state(n_batch, self.nch_h, H, W, self.device)
+        
         # state buffer
         h_buff = ()
         c_buff = ()
@@ -99,12 +104,17 @@ class LSTMModel(nn.Module):
         # the final output
         return h_[-1]
     
-    def init_state(self, nch_h):
+    def init_state(self, n_batch, nch_h, H, W, device):
         h_ = ()
         c_ = ()
         for i in range(len(nch_h)):
-            
-        
+            h0 = torch.empty(n_batch, nch_h[i], H, W).to(device)
+            nn.init.xavier_normal_(h0)
+            c0 = torch.empty(n_batch, nch_h[i], H, W).to(device)
+            nn.init.xavier_normal_(c0)
+            h_ = h_ + (h0,)
+            c_ = c_ + (c0,)
+        return h_, c_
         
         
         
